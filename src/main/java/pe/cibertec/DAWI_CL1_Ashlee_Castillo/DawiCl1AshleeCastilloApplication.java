d329b7dfd5954f840bd3ef9c27103174f57a1de2
sour15 <ashleeagres56@gmail.com>
2024-04-29 08:31:03 -0500

    iniciamos examen

---
+++ src/main/java/pe/cibertec/DAWI_CL1_Ashlee_Castillo/DawiCl1AshleeCastilloApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.DAWI_CL1_Ashlee_Castillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1AshleeCastilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1AshleeCastilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/cibertec/DAWI_CL1_Ashlee_Castillo/DawiCl1AshleeCastilloApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.DAWI_CL1_Ashlee_Castillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1AshleeCastilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
